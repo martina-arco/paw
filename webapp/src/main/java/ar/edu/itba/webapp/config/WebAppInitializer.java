e8e185982c4793c4ea8559e617d3be5cc871fa93
lucas <lucas.emery@hotmail.com>
2019-02-12 22:24:41 -0300

    Fix WebAppInitializer order

--- webapp/src/main/java/ar/edu/itba/webapp/config/WebAppInitializer.java
+++ webapp/src/main/java/ar/edu/itba/webapp/config/WebAppInitializer.java
@@ -2,6 +2,8 @@ package ar.edu.itba.webapp.config;
 
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.servlet.ServletContainer;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -13,15 +15,15 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
-
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class WebAppInitializer implements WebApplicationInitializer {
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
         AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
         appContext.register(WebConfig.class, WebAuthConfig.class);
 
-        FilterRegistration.Dynamic filterRegistration = servletContext.addFilter("springSecurityFilterChain", new DelegatingFilterProxy());
-        filterRegistration.addMappingForUrlPatterns(null, true, "/*");
+        //FilterRegistration.Dynamic filterRegistration = servletContext.addFilter("springSecurityFilterChain", new DelegatingFilterProxy());
+        //filterRegistration.addMappingForUrlPatterns(null, true, "/*");
 
         servletContext.setInitParameter("contextConfigLocation", "<NONE>");
         servletContext.addListener(new ContextLoaderListener(appContext));
@@ -30,6 +32,7 @@ public class WebAppInitializer implements WebApplicationInitializer {
         ServletRegistration.Dynamic servletRegistration = servletContext.addServlet("jersey-servlet", jerseyServlet);
 
         servletRegistration.setInitParameter("jersey.config.server.provider.packages", "ar.edu.itba.webapp.controllers");
+        servletRegistration.setInitParameter("com.sun.jersey.api.json.POJOMappingFeature", "true");
         servletRegistration.setLoadOnStartup(1);
         servletRegistration.addMapping("/*");
     }
