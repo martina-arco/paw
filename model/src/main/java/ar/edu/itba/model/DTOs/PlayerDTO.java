cabc990ac27ac2fc723a11b10d971adc784171c8
Martina Arco <marco@itba.edu.ar>
2019-07-02 20:33:20 -0300

    player position to dto

--- model/src/main/java/ar/edu/itba/model/DTOs/PlayerDTO.java
+++ model/src/main/java/ar/edu/itba/model/DTOs/PlayerDTO.java
@@ -4,14 +4,19 @@ import ar.edu.itba.model.Player;
 
 public class PlayerDTO {
     private final String name, team;
-    private final int goalkeeping, defense, passing, finishing, skillLevel, value, salary, fitness, age, potential;
+    private final int goalkeeping, defense, passing, finishing, skillLevel, value, salary, fitness, age, potential, position;
     private long id;
 
     public PlayerDTO(Player player){
-        this(player.getId(), player.getName(), player.getTeam().getName(), player.getGoalKeeping(), player.getDefending(), player.getPassing(), player.getFinishing(), player.getSkillLevel(), player.getValue(), player.getSalary(), player.getFitness(), player.getAge(), player.getPotential());
+        this(player.getId(), player.getName(), player.getTeam().getName(), player.getGoalKeeping(),
+                player.getDefending(), player.getPassing(), player.getFinishing(), player.getSkillLevel(),
+                player.getValue(), player.getSalary(), player.getFitness(), player.getAge(), player.getPotential(),
+                player.getPosition());
     }
 
-    public PlayerDTO(long id, String name, String team, int goalkeeping, int defense, int passing, int finishing, int skillLevel, int value, int salary, int fitness, int age, int potential) {
+    public PlayerDTO(long id, String name, String team, int goalkeeping, int defense, int passing,
+                     int finishing, int skillLevel, int value, int salary, int fitness, int age,
+                     int potential, int position) {
         this.id = id;
         this.name = name;
         this.team = team;
@@ -25,6 +30,7 @@ public class PlayerDTO {
         this.fitness = fitness;
         this.age = age;
         this.potential = potential;
+        this.position = position;
     }
 
     public int getValue() {
@@ -77,5 +83,9 @@ public class PlayerDTO {
 
     public long getId() {
         return id;
+    }
+
+    public int getPosition() {
+        return position;
     }
 }
