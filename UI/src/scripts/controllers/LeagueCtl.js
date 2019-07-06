01c15f9eea73e9ccb9fd420f197aaa0c8d862064
Martina Arco <marco@itba.edu.ar>
2019-07-06 20:28:55 -0300

    fix league

--- UI/src/scripts/controllers/LeagueCtl.js
+++ UI/src/scripts/controllers/LeagueCtl.js
@@ -2,15 +2,12 @@ define(['footballManager', 'services/LeagueService', 'services/TeamService', 'se
 
     footballManager.controller('LeagueCtl', function ($scope, LeagueService, TeamService, MatchService) {
 
-        MatchService.getUpcomingMatches().then(function (response) {
-          $scope.upcomingMatches = response.data;
-        });
-
         LeagueService.getLeagueInformation().then(function (response) {
           $scope.matchesToPlay = response.data.matchesToPlay;
           $scope.matchesPlayed = response.data.matchesPlayed;
           $scope.league = response.data.league;
           $scope.teams = response.data.teams;
+          $scope.upcomingMatches = response.data.upcomingMatches;
         });
     });
 
