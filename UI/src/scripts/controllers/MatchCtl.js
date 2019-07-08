babcbefd0004c954344f2cd9835b522374d06b17
Martina Arco <marco@itba.edu.ar>
2019-07-07 21:32:01 -0300

    play match

--- UI/src/scripts/controllers/MatchCtl.js
+++ UI/src/scripts/controllers/MatchCtl.js
@@ -1,58 +1,62 @@
 define(['footballManager', 'services/MatchService'], function (footballManager) {
 
-    footballManager.controller("MatchCtl", function ($scope, $location, MatchService) {
+    footballManager.controller("MatchCtl", function ($scope, $location, $timeout, MatchService) {
 
         $scope.matches = [];
         $scope.time = 0;
+        $scope.matchesWithEvents = [];
 
         MatchService.playMatch().then(function (response) {
-            var matchesWithEvents = response.data;
+            $scope.matchesWithEvents = response.data;
 
-            matchesWithEvents.forEach(function (match) {
-                var matchToAdd = {id: match.id, stadium: match.stadium, homeTeam: match.homeTeam,
-                  awayTeam: match.awayTeam, homeScore: 0, awayScore: 0, event:''};
+            $scope.matchesWithEvents.forEach(function (match) {
+                var matchToAdd = {id: match.id, stadium: match.stadium, home: match.home,
+                  away: match.away, homeScore: 0, awayScore: 0, event:''};
+
                 $scope.matches.push(matchToAdd);
             })
         });
 
-        function iterate(){
+        var iterate = function() {
 
-          if($scope.time > 90){
-            $location.url('/matchEnd');
-            return;
-          }
+            if($scope.time > 90){
+                $location.url('/matchEnd');
+                return;
+            }
 
-          $scope.matches.forEach(function (match) {
-            match.events.forEach(function (event) {
-              if(event.minute === minute){
-                updateEvents(match, event);
-              }
-            });
-          });
+            for (var i = 0; i < $scope.matchesWithEvents.length; i++)  {
+                var match = $scope.matchesWithEvents[i];
+                var oldMatch = $scope.matches[i];
 
-          // jQuery('.progress-bar').css('width', (minute * 1.1) + '%');
+                match.events.forEach(function (event) {
+                    if(event.minute === $scope.time){
+                        updateEvents(oldMatch, event);
+                    }
+                });
+            }
 
-          $scope.time++;
-          setTimeout(iterate, 150, matches, time);
-        }
+            // jQuery('.progress-bar').css('width', (minute * 1.1) + '%');
 
-        function updateEvents(match, event){
-          var eventString;
+            $scope.time++;
+            $timeout(iterate, 150);
+        };
+
+        var updateEvents = function(match, event){
 
           switch(event.type) {
             case "HOMESCORE":
-              eventString = event.player1 + " - " + "goal scored" + "( " + event.minute + "' )";
+              match.event = event.player1 + " - " + "goal scored" + "( " + event.minute + "' )";
               match.homeScore++;
               break;
             case "AWAYSCORE":
-              eventString = event.player1 + " - " + "goal scored" + "( " + event.minute + "' )";
+              match.event = event.player1 + " - " + "goal scored" + "( " + event.minute + "' )";
               match.awayScore++;
               break;
             case "YELLOW CARD":
-              eventString = event.player1 + " - " + "yellow card" + " ( " + event.minute + " )";
+              match.event = event.player1 + " - " + "yellow card" + " ( " + event.minute + " )";
               break;
             case "RED CARD":
-              eventString = event.player1 + " - " + "red card" + " ( " + event.minute + " )";
+              match.event = event.player1 + " - " + "red card" + " ( " + event.minute + " )";
               break;
             // case "SUBSTITUTE":
 
@@ -62,12 +66,10 @@ define(['footballManager', 'services/MatchService'], function (footballManager) 
               break;
           }
 
-          match.event = eventString;
+          return match;
+        };
 
-        }
-
-        iterate();
-
+        $timeout(iterate, 150);
 
     });
 
